9aefc60f1cfce398756411717a1fb7c90768fa07
jeremie <nerom95@gmail.com>
2025-03-30 06:34:16 +0200

    Hero : creation

---
+++ src/app/components/sections/Hero.tsx
@@ -0,0 +1,60 @@
+import datas from "../../datas.json";
+import { CustomBtn, IconName } from "../buttons/custom-buttons";
+import ToolTip from "../ui/ToolTip";
+
+export default function Hero() {
+  return (
+    <section
+      id="home"
+      className="relative w-full h-screen flex flex-col items-center justify-center gap-12 max-md:-top-10"
+    >
+      <div className="absolute top-0 background-hero w-full h-full flex items-center -z-1">
+        <div className="background-hero-circle w-full h-full"></div>
+      </div>
+
+      <div className="flex flex-col gap-2 max-md:px-4">
+        <h1 className="min-md:text-7xl text-4xl text-center">
+          {datas.home.title}
+        </h1>
+        <h2 className="text-primary min-md:text-7xl text-4xl tracking-wider text-center">
+          {datas.home.subtitle}
+        </h2>
+      </div>
+
+      <p className="min-sm:w-2/3 max-md:px-6 min-md:text-xl text-sm text-center">
+        {datas.home.description}
+      </p>
+
+      <div className="flex gap-6 flex-wrap">
+        <CustomBtn
+          href="/CV-Jeremie-Herault.pdf"
+          theme="primary"
+          iconName="download"
+          download={"CV-Jeremie-Herault.pdf"}
+        >
+          Mon CV
+        </CustomBtn>
+        <div className="flex items-center gap-2 ">
+          {datas.home.buttons.map((button, idx) => {
+            return (
+              <span key={idx} className={`relative z-20 group`}>
+                <CustomBtn
+                  //key={button.title}
+                  href={button.href}
+                  theme="outline"
+                  size="md"
+                  target={button.target}
+                  iconName={button.iconName as IconName}
+                  ariaLabel={`Aller sur ${button.title}`}
+                >
+                  <span className="sr-only">{button.title}</span>
+                </CustomBtn>
+                <ToolTip txt={button.title} color="none" />
+              </span>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
