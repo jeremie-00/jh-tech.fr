3c3e993ba677c65db700b663a8aba2e7e360fd1b
jeremie <nerom95@gmail.com>
2025-03-30 06:39:45 +0200

    toast : ajout des toast de confirmation des email

---
+++ src/app/components/ui/toast.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { Flip, toast, ToastContainer } from "react-toastify";
+import { Result } from "../../types/globalType";
+
+export const showToast = (
+  type: "success" | "error" | "warn" | "info",
+  message: string
+) => {
+  toast[type](message, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "colored",
+  });
+};
+
+export const ToastNotification = () => (
+  <ToastContainer
+    position="bottom-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick={false}
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="colored"
+    transition={Flip}
+  />
+);
+
+export const handleResponseToast = <T,>(response: Result<T>) => {
+  const { data, serverError, validationErrors } = response;
+
+  if (data && data.success) {
+    const { status, message } = data;
+    showToast(status, message);
+    return true;
+  }
+
+  if (data && !data.success) {
+    const { status, message } = data;
+    showToast(status, message);
+    return false;
+  }
+
+  if (serverError) {
+    showToast("error", serverError);
+    return false;
+  }
+
+  if (validationErrors) {
+    const firstErrorKey = Object.keys(validationErrors)[0];
+    const firstError = validationErrors[firstErrorKey]?._errors?.[0];
+
+    if (firstError) {
+      showToast("warn", firstError);
+    }
+    return false;
+  }
+
+  return false;
+};
