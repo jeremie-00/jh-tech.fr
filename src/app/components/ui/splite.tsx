e032ff08df73148cb5e6efbb11f253a6071fc8d9
jeremie <nerom95@gmail.com>
2025-04-16 13:36:45 +0200

    modification import spline

--- src/app/components/ui/splite.tsx
+++ src/app/components/ui/splite.tsx
@@ -3,14 +3,17 @@
 import dynamic from "next/dynamic";
 import FadeInSection from "./FadeInSection";
 import { LoaderBars } from "./Loader";
-const Spline = dynamic(() => import("@splinetool/react-spline"), {
-  ssr: false, // Désactive le rendu côté serveur
-  loading: () => (
-    <div className="w-1/2 h-full flex items-center justify-center">
-      <LoaderBars size={54} />
-    </div>
-  ), // Composant de fallback
-});
+const Spline = dynamic(
+  () => import("@splinetool/react-spline").then((mod) => mod.default),
+  {
+    ssr: false, // Désactive le rendu côté serveur
+    loading: () => (
+      <div className="w-1/2 h-full flex items-center justify-center">
+        <LoaderBars size={54} />
+      </div>
+    ), // Composant de fallback
+  }
+);
 
 interface SplineSceneProps {
   scene: string;
